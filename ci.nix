fb8cd349dd906681f300ade3da0f18eac23bf9e5
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-10-24 19:53:20 -0400

    get my packages

--- ci.nix
+++ ci.nix
@@ -4,7 +4,18 @@ let
     url = https://github.com/nixos/nixpkgs/;
     rev = "4cd2cb43fb3a87f48c1e10bb65aee99d8f24cb9d";
   }) {};
+  inherit (nixpkgs) lib;
+
 in
-  {
-    inherit (nixpkgs) hello;
-  }
+  lib.filterAttrs (
+    pname: pkg:
+      let
+        eval = builtins.tryEval pkg;
+        meta = (builtins.tryEval (eval.value.meta or { maintainers = []; })).value or {};
+      in
+        if eval.success then
+          builtins.any
+            (m: (m.github or null) == "veprbl")
+            (meta.maintainers or [])
+        else
+          false) nixpkgs.pkgs
