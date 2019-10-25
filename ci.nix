9b2ab2a68ac725c172768c5dea8f2c9a37ec34e3
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2019-10-24 22:02:23 -0400

    switch to nixos-19.09

--- ci.nix
+++ ci.nix
@@ -1,8 +1,9 @@
 let
   nixpkgs = import (builtins.fetchGit {
-    name = "nixos-unstable-2019-10-24";
+    name = "nixos-19.09-2019-10-24";
     url = https://github.com/nixos/nixpkgs/;
-    rev = "4cd2cb43fb3a87f48c1e10bb65aee99d8f24cb9d";
+    rev = "f6dac8083874408fe287525007d3da9decd9bf44";
+    ref = "release-19.09";
   }) {};
   inherit (nixpkgs) lib;
 
